80bb270b7fec7eeb45479c166c13bdb190c63f3f
metapsj <patrickjohnson@forty3sixteen.com>
2021-09-28 14:09:39 -0700

    include delete option

--- script/deploy-via-rsync.sh
+++ script/deploy-via-rsync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # rsync -av --delete --rsh=ssh ./dist/* RodgersAutoRepair:/home/rodgqwoo/public_html
-rsync -aP ./dist/ RodgersAutoRepair:/home/rodgqwoo/public_html
+rsync -aP --delete ./dist/ RodgersAutoRepair:/home/rodgqwoo/public_html/
 
