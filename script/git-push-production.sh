316270521d6be741c35a6b27f4250081df1201cb
metapsj <patrickjohnson@forty3sixteen.com>
2021-09-28 11:07:36 -0700

    add scripts for ssh'g to production and git push'g to production

---
+++ script/git-push-production.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# setup
+# git remote add production ssh://RodgersAutoRepair/home/rodgqwoo/rodgers-auto-repair
+# git config remote.production.receivepack /usr/local/cpanel/3rdparty/git-receive-pack
+# git config receive.denyCurrentBrach ignore
+# cat .git/config
+# git push - set-upstream production main
+
+git push production
+
---
+++ script/ssh-rodgers-auto-repair.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# ssh -p 21098 rodgqwoo@server275.web-hosting.com
+ssh -p 21098 rodgqwoo@rodgersautorepair.com
+
