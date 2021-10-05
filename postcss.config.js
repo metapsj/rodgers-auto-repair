d5c9ed63a3566f2d0289ef31a83757a7ad131103
metapsj <patrickjohnson@forty3sixteen.com>
2021-10-05 10:41:29 -0700

    change postcss destination to public/assets/webfonts

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./index.html"],
+  content: ["./src/index.html"],
   // This is the function used to extract class names from your templates
   defaultExtractor: (content) => {
     // Capture as liberally as possible, including things like `h-(screen-1.5)`
@@ -19,7 +19,7 @@ module.exports = {
     require("tailwindcss")(),
     require("autoprefixer")(),
     require("postcss-copy")({
-      dest: "assets/webfonts",
+      dest: "public/assets/webfonts",
     }),
     ...(process.env.ENV_BUILD === "prod" ? [purgecss] : []),
     ...(process.env.ENV_BUILD === "prod" ? [require("cssnano")()] : []),
